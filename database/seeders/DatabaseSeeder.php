d7dd0b69ef2dbf1131f2d296af79f7e0093a2fc2
Younes Mahmoud <youns3510@gmail.com>
2024-09-27 00:28:37 +0300

    uncommit seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    // $this->call([
-    //   RoleSeeder::class,
-    //   PermissionSeeder::class
-    // ]);
+    $this->call([
+      RoleSeeder::class,
+      PermissionSeeder::class
+    ]);
 
     // User::factory(10)->create();
 
