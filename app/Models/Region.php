8bdf77bf2818d025cb0b41ddd085e6d0933f8291
Venson Estrope <vensonestrope13@gmail.com>
2024-10-10 14:51:52 +0800

    Fixedin Region Observer

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\RegionObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+#[ObservedBy([RegionObserver::class])]
 class Region extends Model
 {
     use HasFactory;
