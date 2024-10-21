2217f130b04d4bec98fede495a8f5a267b3e16e6
Younes Mahmoud <youns3510@gmail.com>
2024-10-22 01:28:53 +0300

    disable csrf

--- resources/views/components/meta-tags.blade.php
+++ resources/views/components/meta-tags.blade.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8" />
-<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+{{-- <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> --}}
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="csrf-token" content="{{ csrf_token() }}">
+{{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
 <title>{{ $title ?? config('app.name') }} - {{ config('app.name') }} </title>
 <meta name="description"
