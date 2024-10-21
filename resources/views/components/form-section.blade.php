ea6cb7ae9bf15b6938cb4b2ab4dab6d3a93caa6b
Younes Mahmoud <youns3510@gmail.com>
2024-10-22 00:35:29 +0300

    add csrf to form

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -8,6 +8,7 @@
 
     <div class="mt-5 md:mt-0 md:col-span-2">
         <form wire:submit="{{ $submit }}">
+          @csrf
             <div class="px-4 py-5 bg-white sm:p-6 shadow {{ isset($actions) ? 'sm:rounded-tl-md sm:rounded-tr-md' : 'sm:rounded-md' }}">
                 <div class="grid grid-cols-6 gap-6">
                     {{ $form }}
