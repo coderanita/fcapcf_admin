542b22aabb632c57306f0662b4015f6c43aae9f6
Venson Estrope <vensonestrope13@gmail.com>
2024-10-23 21:04:21 +0800

    Remove

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,11 +45,11 @@
                 <footer class="d-flex flex-wrap justify-content-between align-items-center py-2">
                     <p class="col-md-5 mb-0 text-muted">© 2024 <a href="javascript:void(0)">First Child & Prisoner Care
                             Foundation</a>. <span class="fa fa-heart text-danger"></span> </p>
-                    <ul class="nav col-md-7 justify-content-end">
+                    {{-- <ul class="nav col-md-7 justify-content-end">
                         <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
                         <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Support</a></li>
                         <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Purchase</a></li>
-                    </ul>
+                    </ul> --}}
                 </footer>
             </div>
         </div>
