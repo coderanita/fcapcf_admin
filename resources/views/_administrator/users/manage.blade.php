369abc882fe5db86b1d17674e3d6d03c897f855a
Venson Estrope <vensonestrope13@gmail.com>
2024-10-25 02:12:15 +0800

    Remove Project in Table

--- resources/views/_administrator/users/manage.blade.php
+++ resources/views/_administrator/users/manage.blade.php
@@ -6,7 +6,7 @@
                 <th>S/N</th>
                 <th>Name</th>
                 <th>Account Type</th>
-                <th>Assigned Program</th>
+                {{-- <th>Assigned Program</th> --}}
                 <th>Status</th>
                 <th>Action</th>
             </tr>
@@ -22,7 +22,7 @@
                     <td> {{ $loop->iteration }}</td>
                     <td>{{ $user->fullName() }} <br> <span class="text-info">{{ $user->email }}</span></td>
                     <td>{{ ucwords($user->role->name) }}</td>
-                    <td><span class="badge bg-primary">MCA - Nasarawa</span></td>
+                    {{-- <td><span class="badge bg-primary">MCA - Nasarawa</span></td> --}}
                     <td> <button title="Click To Change" wire:click="toggleActive({{ $user->id }})"
                             class="badge  @if ($user->active) bg-success"> Active @else bg-danger"> Suspend @endif
         </button></td>
