9e114c2d801d3a32584a7f7b002ae73a744f3505
Venson Estrope <vensonestrope13@gmail.com>
2024-12-06 11:08:58 +0800

    Add Search

--- resources/views/_administrator/project-category/manage.blade.php
+++ resources/views/_administrator/project-category/manage.blade.php
@@ -128,4 +128,28 @@
             $('#edit_project_category_modal').modal('show');
         })
     </script>
+
+    <script src="{{ asset('js/theme.js') }}"></script>
+    <!-- Plugin Js -->
+    <script src="{{ asset('js/bundle/dataTables.bundle.js') }}"></script>
+    <!-- Jquery Page Js -->
+    <script>
+        $('.myDataTable').addClass('nowrap').dataTable({
+            responsive: true,
+            searching: true,
+            paging: false,
+            ordering: true,
+            info: false,
+        });
+        $('#myDataTable_no_filter').addClass('nowrap').dataTable({
+            responsive: true,
+            searching: false,
+            paging: false,
+            ordering: false,
+            info: false,
+        });
+        $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function(e) {
+            $($.fn.dataTable.tables(true)).DataTable().columns.adjust().responsive.recalc();
+        });
+    </script>
 @endpush
